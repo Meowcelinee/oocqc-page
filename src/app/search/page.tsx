2bcde9f6b59f888c2a3b2c879653ee8938855c97
Marceline Raine <meowcelinee@proton.me>
2025-02-06 18:46:07 -0500

    quote form

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,18 @@
+import QuoteForm from './QuoteForm';
+
 export default function SearchPage() {
-    return <>search page</>;
+    return (
+        <>
+            <div className='flex-col md:py-4 py-2'>
+                <h1 className='md:my-6 my-4 md:text-4xl text-3xl text-center font-semibold drop-shadow-lg'>
+                    Find a quote!
+                </h1>
+                <div className='flex justify-center gap-3 bg-mantle px-4 py-2 w-full'>
+                    <div className='px-2 py-2 md:w-3/4 w-full'>
+                        <QuoteForm />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
