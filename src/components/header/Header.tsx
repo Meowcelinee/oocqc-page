ac6fff591288b79c6b8d7f74251ded85c4ca96eb
Marceline Raine <nyatalieeee@proton.me>
2025-02-10 21:09:00 -0500

    improve header spacing

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,21 +6,19 @@ import NavbarLinks from './NavbarLink';
 
 export default function Header() {
     return (
-        <>
-            <header className='flex md:px-2 px-1 py-2'>
-                <Link href='/'>
-                    <Image
-                        width={176}
-                        height={43}
-                        src='/images/logos/icon.png'
-                        alt=''
-                        className='mr-4 transition duration-200 hover:brightness-75 hover:opacity-90'
-                    />
-                </Link>
-                <div className='flex my-auto'>
-                    <NavbarLinks />
-                </div>
-            </header>
-        </>
+        <header className='flex justify-start md:gap-0 gap-3 md:px-2 px-1 py-2'>
+            <Link href='/'>
+                <Image
+                    width={176}
+                    height={43}
+                    src='/images/logos/icon.png'
+                    alt=''
+                    className='mr-4 transition duration-200 hover:brightness-75 hover:opacity-90'
+                />
+            </Link>
+            <div className='flex my-auto'>
+                <NavbarLinks />
+            </div>
+        </header>
     );
 }
