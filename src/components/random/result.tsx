4311027681bccf337ff4c48d1d6abc05f2454cc1
Marceline Raine <nyatalieeee@proton.me>
2025-02-15 19:41:42 -0500

    link-ify

--- src/components/random/result.tsx
+++ src/components/random/result.tsx
@@ -1,14 +1,20 @@
 import { Quote } from '@/data/quotes/quotes';
+import Link from 'next/link';
 
 export default function RandomResult(props: Quote) {
     return (
-        <div className='bg-gradient-to-r from-surface0 to-base rounded-r-lg border-l-4 border-l-surface2 drop-shadow-lg my-4 p-3'>
-            <h2 className='md:text-2xl text-xl text-subtext1 font-semibold'>
-                "{props?.quote}"
-            </h2>
-            <p className='text-overlay2 md:text-lg italic'>
-                -- {props?.person}
-            </p>
-        </div>
+        <Link
+            href={`/view/${props.id}`}
+            className='transition duration-300 hover:brightness-125'
+        >
+            <div className='bg-gradient-to-r from-surface0 to-base rounded-r-lg border-l-4 border-l-surface2 drop-shadow-lg my-4 p-3'>
+                <h2 className='md:text-2xl text-xl text-subtext1 font-semibold'>
+                    "{props?.quote}"
+                </h2>
+                <p className='text-overlay2 md:text-lg italic'>
+                    -- {props?.person}
+                </p>
+            </div>
+        </Link>
     );
 }
