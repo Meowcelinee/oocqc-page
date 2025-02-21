1d3324b4f6c3f5aeb1c30ad61369c8a2644911a3
Marceline Raine <nyatalieeee@proton.me>
2025-02-21 10:33:10 -0500

    fix underscores for specific quote attribute

--- src/data/quotes/quotes.ts
+++ src/data/quotes/quotes.ts
@@ -5,8 +5,14 @@ type Quote = { quote: string; person: string; id: number };
 const validQuotes = rawQuotes.map(
     (quote: string, quoteIndex: number): Quote => {
         const splitQuote = quote.split(' -');
+
         const finalQuote = splitQuote[0].replaceAll(`"`, ``);
-        const finalAttribute = splitQuote[1].replaceAll(`_`, ` `);
+
+        const finalAttribute =
+            splitQuote[1] !== 'net.minecraft.server.events.PLAYER_DIES'
+                ? splitQuote[1].replaceAll(`_`, ` `)
+                : splitQuote[1];
+
         return {
             quote: finalQuote,
             person: finalAttribute,
